c2ec4f17b49c0b3c64f2f3186f3a12418204548b
rybka <iczekajski@gmail.com>
2023-11-14 20:15:59 +0100

    incredibly in your face

--- kamien papier nozyce.py
+++ kamien papier nozyce.py
@@ -1,7 +1,26 @@
 import random
+import time
 print("Gra w kamien papier nozyce")
-opcje= ["kamien", "papier", "nozyce"] 
-wybor_gracza=input("ka-mien pa-pier no-zy-ce...")
-wybor_komputera=random.randrange["kamien", "papier", "nozyce"]
+
+time.sleep(1)
+
+opcje=["kamien","papier","nozyce"] 
+
+wybor_gracza=input("ka-mien pa-pier no-zy-ce..?")
+wybor_komputera=random.choice(opcje)
+
 if wybor_gracza=="kamien" and wybor_komputera=="kamien":
-    print("remis!")
+    print("kamien vs kamien")
+    time.sleep(1)
+    print("Remis!")
+elif wybor_gracza=="kamien" and wybor_komputera=="papier":
+     print("kamien vs papier")
+     time.sleep(1)
+     print("Wygrales!")
+elif wybor_gracza=="kamien" and wybor_komputera=="nozyce":
+    print("kamien vs nozyce")
+    time.sleep(1)
+    print("Przegrałeś")
+
+    
+print("dziekuje za gre fani")
