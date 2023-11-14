c6c7745ec8c52f5cd0ae5b81ab43c2bd5ce04aef
rybka <iczekajski@gmail.com>
2023-11-14 19:41:00 +0100

    .

--- kamien papier nozyce.py
+++ kamien papier nozyce.py
@@ -1,8 +1,7 @@
 import random
 print("Gra w kamien papier nozyce")
-opcje= [ kamien, papier, nozyce ] 
-print(opcje)
-wybor_gracza=int(input("kamien/papier/nozyce"))
+opcje= ["kamien", "papier", "nozyce"] 
+wybor_gracza=input("ka-mien pa-pier no-zy-ce...")
 wybor_komputera=random.randrange["kamien", "papier", "nozyce"]
 if wybor_gracza=="kamien" and wybor_komputera=="kamien":
     print("remis!")
