f481b46427b3c080df77a56d2052fd4d7c5806d7
rybka <iczekajski@gmail.com>
2023-12-05 09:07:56 +0100

    .

--- projekty/weryfikacja numeru pesel.py
+++ projekty/weryfikacja numeru pesel.py
@@ -1,12 +1,9 @@
-""" numer_pesel=int(input("Twój numer PESEL to: ..."))
-
-cyfry=[0,1,2,3,4,5,6,7,8,9]
-
-if len(numer_pesel)==11:
-    print("ur mom") """
+import time
 
 numer_pesel = input("Twój numer PESEL to: ...")
 
-if not len(numer_pesel) == 11 and numer_pesel.isdigit():
-    print("Błąd! Numer PESEL powinien składać się z 11 cyfr.")
+if len(numer_pesel) == 11 and numer_pesel.isdigit():
+    print("Numer PESEL jest poprawny")
+else:
+    print("Twoj numer PESEL jest niepoprawny, powinien sie skladac z 11 cyfr")
 
