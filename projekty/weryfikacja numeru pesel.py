6f166325ad88a90153175f1160a728cc2e031210
rybka <iczekajski@gmail.com>
2023-12-05 10:24:55 +0100

    .

--- projekty/weryfikacja numeru pesel.py
+++ projekty/weryfikacja numeru pesel.py
@@ -11,15 +11,15 @@ if len(numer_pesel) == 11 and numer_pesel.isdigit():
         print("Jestes plci meskiej")
     elif int(numer_pesel[9]) %2==0:
         print("Jestes plci zenskiej")
-    if int(numer_pesel[2,3]) in wiek20:
-        print("Urodziles/as sie w 20 wieku")
-    elif int(numer_pesel[2,3]) in wiek21:
-        print("Urodziles/as sie w 21 wieku")
+    if int(numer_pesel[2]) in {0,1}:
+        print(f"Twoja data urodzenia to {numer_pesel[4]}{numer_pesel[5]}.{numer_pesel[2]}{numer_pesel[3]}.10{numer_pesel[0]}{numer_pesel[1]}")
+    elif int(numer_pesel[2]) in{2,3}:
+        print(f"Twoja data urodzenia to               efwhufhuiewhufiewuhifewhuifeuwhfuhiwfiuhewih       ")
     else:
         print("Twoj numer pesel nie isnieje")
 
 
 
 
-else:
+else:   
     print("Twoj numer PESEL jest niepoprawny, powinien sie skladac z 11 cyfr")
