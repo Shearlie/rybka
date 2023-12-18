78cacf5d8145c8872998d9097cab4dafa1062823
rybka <iczekajski@gmail.com>
2023-12-18 21:35:25 +0100

    .

--- projekty/weryfikacja numeru pesel.py
+++ projekty/weryfikacja numeru pesel.py
@@ -1,25 +1,26 @@
 import time
 
-numer_pesel = input("Twój numer PESEL to: ... ")
+numer_pesel = input("Twój numer PESEL to: ... ? ")
 wiek20=list(range(0,13))
 wiek21=list(range(20,33))
 
-if len(numer_pesel) == 11 and numer_pesel.isdigit():
-    print("Numer PESEL jest poprawny")
+if len(numer_pesel) == 11 and numer_pesel.isdigit() and int(numer_pesel[2]) == 0 or 1 or 2 or 3:
+    print("Numer PESEL powinien być poprawny")
     time.sleep(1)
     if  int(numer_pesel[9]) %2==1:
-        print("Jestes plci meskiej")
+        print("Jestes mezczyzna")
     elif int(numer_pesel[9]) %2==0:
-        print("Jestes plci zenskiej")
+        print("Jestes kobieta")
+    time.sleep(1)
     if int(numer_pesel[2]) in {0,1}:
-        print(f"Twoja data urodzenia to {numer_pesel[4]}{numer_pesel[5]}.{numer_pesel[2]}{numer_pesel[3]}.10{numer_pesel[0]}{numer_pesel[1]}")
+        print(f"Twoja data urodzenia to {numer_pesel[4]}{numer_pesel[5]}.{numer_pesel[2]}{numer_pesel[3]}.19{numer_pesel[0]}{numer_pesel[1]}")
     elif int(numer_pesel[2]) in{2,3}:
-        print(f"Twoja data urodzenia to               efwhufhuiewhufiewuhifewhuifeuwhfuhiwfiuhewih       ")
+        print(f"Twoja data urodzenia to {numer_pesel[4]}{numer_pesel[5]}.{int(numer_pesel[2])-2}{numer_pesel[3]}.20{numer_pesel[0]}{numer_pesel[1]}")
     else:
         print("Twoj numer pesel nie isnieje")
 
-
+    time.sleep(1)
 
 
 else:   
-    print("Twoj numer PESEL jest niepoprawny, powinien sie skladac z 11 cyfr")
+    print("Twoj numer PESEL jest niepoprawny, sprawdz czy sklada sie z 11 cyfr")
